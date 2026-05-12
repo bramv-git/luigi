a3a61a4410a255880e1479e42d7a7fb248f87dbf
bramv <bram.verschelde@hotmail.com>
2026-05-12 15:34:56 +0200

    SpringFundamentals17 - 10 Thymeleaf

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -1,17 +1,18 @@
 package be.vdab.luigi.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalTime;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexController {
     @GetMapping
-    public String index() {
+    public ModelAndView index() {
         var morningOrAfternoon = LocalTime.now().getHour() < 12 ? "morning" : "afternoon";
-        return "<!doctype html><html><title>Hallo</title><body>Good "+morningOrAfternoon+"</body></html>";
+        return new ModelAndView("index", "time", morningOrAfternoon);
     }
 }
